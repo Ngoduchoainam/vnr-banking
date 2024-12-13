971f15628905ab9666767c95d69903f638086117
unknown <nam96ht@gmail.com>
2024-12-13 09:48:14 +0700

    them log 2

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,57 +1,45 @@
-"use client";
-
 import LayoutWapper from "@/src/component/LayoutWapper";
 import { RoleWpparProvidrer } from "@/src/component/RoleWapper";
 import React from "react";
-// import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
+import { auth } from "../api/auth/[...nextauth]/config";
+import { redirect } from "next/navigation";
 
-const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  // const { data: session, status } = useSession();
-  // const [data, setData] = useState<any>(null);
-  // const [loading, setLoading] = useState(true);
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     if (!session?.user?.access_token) {
-  //       router.push("/login");
-  //       return;
-  //     }
-
-  //     try {
-  //       const response = await fetch(
-  //         "https://apiweb.bankings.vnrsoftware.vn/account/find-role-by-account",
-  //         {
-  //           method: "GET",
-  //           headers: {
-  //             Authorization: session.user.access_token,
-  //           },
-  //         }
-  //       );
-  //       const result = await response.json();
-  //       setData(result.data);
-  //     } catch (error) {
-  //       console.error("Failed to fetch roles:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   if (status === "authenticated") {
-  //     fetchData();
-  //   }
-  // }, [session, status, router]);
-
-  // if (status === "loading" || loading) {
-  //   return <div>Loading...</div>; // Or a custom loading component
-  // }
+const RootLayout: React.FC<{ children: React.ReactNode; data: any }> = ({
+  children,
+  data,
+}) => {
+  if (!data) {
+    redirect("/login");
+    return null; // Prevent rendering anything if redirected
+  }
 
   return (
     <LayoutWapper>
-      <RoleWpparProvidrer>{children}</RoleWpparProvidrer>
+      <RoleWpparProvidrer data={data.data}>{children}</RoleWpparProvidrer>
     </LayoutWapper>
   );
 };
+
+export async function getServerSideProps() {
+  const session = await auth();
+  if (!session?.user?.access_token) {
+    redirect("/login");
+    return { props: { data: null } }; // Early exit if no session
+  }
+
+  const response = await fetch(
+    "https://apiweb.bankings.vnrsoftware.vn/account/find-role-by-account",
+    {
+      method: "GET",
+      headers: {
+        Authorization: session?.user?.access_token ?? "",
+      },
+    }
+  );
+
+  const data = response ? await response.json() : undefined;
+
+  return { props: { data } };
+}
 
 export default RootLayout;
