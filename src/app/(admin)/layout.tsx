37869431edddb5cbbc71213c506e19e9d4e0e2ff
unknown <nam96ht@gmail.com>
2024-12-13 09:39:39 +0700

    them log 2

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -2,54 +2,54 @@
 
 import LayoutWapper from "@/src/component/LayoutWapper";
 import { RoleWpparProvidrer } from "@/src/component/RoleWapper";
-import React, { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import React from "react";
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/navigation";
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { data: session, status } = useSession();
-  const [data, setData] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
+  // const { data: session, status } = useSession();
+  // const [data, setData] = useState<any>(null);
+  // const [loading, setLoading] = useState(true);
+  // const router = useRouter();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!session?.user?.access_token) {
-        router.push("/login");
-        return;
-      }
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     if (!session?.user?.access_token) {
+  //       router.push("/login");
+  //       return;
+  //     }
 
-      try {
-        const response = await fetch(
-          "https://apiweb.bankings.vnrsoftware.vn/account/find-role-by-account",
-          {
-            method: "GET",
-            headers: {
-              Authorization: session.user.access_token,
-            },
-          }
-        );
-        const result = await response.json();
-        setData(result.data);
-      } catch (error) {
-        console.error("Failed to fetch roles:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  //     try {
+  //       const response = await fetch(
+  //         "https://apiweb.bankings.vnrsoftware.vn/account/find-role-by-account",
+  //         {
+  //           method: "GET",
+  //           headers: {
+  //             Authorization: session.user.access_token,
+  //           },
+  //         }
+  //       );
+  //       const result = await response.json();
+  //       setData(result.data);
+  //     } catch (error) {
+  //       console.error("Failed to fetch roles:", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    if (status === "authenticated") {
-      fetchData();
-    }
-  }, [session, status, router]);
+  //   if (status === "authenticated") {
+  //     fetchData();
+  //   }
+  // }, [session, status, router]);
 
-  if (status === "loading" || loading) {
-    return <div>Loading...</div>; // Or a custom loading component
-  }
+  // if (status === "loading" || loading) {
+  //   return <div>Loading...</div>; // Or a custom loading component
+  // }
 
   return (
     <LayoutWapper>
-      <RoleWpparProvidrer data={data}>{children}</RoleWpparProvidrer>
+      <RoleWpparProvidrer>{children}</RoleWpparProvidrer>
     </LayoutWapper>
   );
 };
