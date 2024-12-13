2a5139a25c58fa768cc6c4c0ede1dd847621bcb0
unknown <nam96ht@gmail.com>
2024-12-13 09:49:52 +0700

    them log 2

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -4,27 +4,11 @@ import React from "react";
 import { auth } from "../api/auth/[...nextauth]/config";
 import { redirect } from "next/navigation";
 
-const RootLayout: React.FC<{ children: React.ReactNode; data: any }> = ({
-  children,
-  data,
-}) => {
-  if (!data) {
-    redirect("/login");
-    return null; // Prevent rendering anything if redirected
-  }
-
-  return (
-    <LayoutWapper>
-      <RoleWpparProvidrer data={data.data}>{children}</RoleWpparProvidrer>
-    </LayoutWapper>
-  );
-};
-
-export async function getServerSideProps() {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
+
   if (!session?.user?.access_token) {
     redirect("/login");
-    return { props: { data: null } }; // Early exit if no session
   }
 
   const response = await fetch(
@@ -37,9 +21,13 @@ export async function getServerSideProps() {
     }
   );
 
-  const data = response ? await response.json() : undefined;
+  const data = response.ok ? await response.json() : undefined;
 
-  return { props: { data } };
-}
+  return (
+    <LayoutWapper>
+      <RoleWpparProvidrer data={data?.data}>{children}</RoleWpparProvidrer>
+    </LayoutWapper>
+  );
+};
 
 export default RootLayout;
